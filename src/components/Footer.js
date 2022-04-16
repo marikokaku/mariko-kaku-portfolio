45e08790c43045c32abd97a97dbbb7e7d769e833
Mariko <mkaku98@gmail.com>
2022-04-15 21:10:27 -0700

    footer fixed and about photo updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
     return (
-        <div className="footer-div text-center">
-            <p className='footer-text'>&copy; Mariko Kaku All Rights Reserved</p>
+        <div className="fixed-bottom text-center">
+            <p>&copy; Mariko Kaku All Rights Reserved</p>
         </div>
 
     );
--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ function About() {
     return (
         <section>
             <h1 id="about">hi, my names mariko</h1>
-            <img className="img" src={avatar} alt='avatar'/>
+            <img className="img rounded-circle" src={avatar} alt='avatar'/>
             <p>Information Systems Graduate with U.C. Berkeley Coding Full Stack Certification</p>
         </section>
     )
